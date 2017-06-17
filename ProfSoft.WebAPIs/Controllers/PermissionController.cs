1a162d70d058cf3305861462f1f4d880b9d05172
Administrator <Administrator@ADMINISTRATORS>
2017-06-17 18:23:12 +0700

    Modify

--- ProfSoft.WebAPIs/Controllers/PermissionController.cs
+++ ProfSoft.WebAPIs/Controllers/PermissionController.cs
@@ -13,12 +13,26 @@ namespace ProfSoft.WebAPIs.Controllers
     {
         [HttpGet]
         [Route("GetList")]
-        public IEnumerable<Permission> GetList()
+        public List<DTO.Permission> GetList()
         {
+            List<DTO.Permission> listDto = null;
             using (ProfSoftEntities context = new ProfSoftEntities())
             {
-                return context.Permissions.ToList();
+                var listItem = context.Permissions.OrderBy(o => o.Id).ToList<Permission>();
+                if (listItem.Count > 0)
+                {
+                    listDto = new List<DTO.Permission>();
+                    foreach (var item in listItem)
+                    {
+                        DTO.Permission dto = new DTO.Permission();
+                        dto.Id = item.Id;
+                        dto.Code = item.Code;
+                        dto.Name = item.Name;
+                        listDto.Add(dto);
+                    }
+                }
             }
+            return listDto;
         }
     }
 }
