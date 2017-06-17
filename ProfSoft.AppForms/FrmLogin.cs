6f590ae8db1a265ded1c640003fb807752b52eb2
Administrator <Administrator@ADMINISTRATORS>
2017-06-17 17:45:43 +0700

    Add DTO

--- ProfSoft.AppForms/FrmLogin.cs
+++ ProfSoft.AppForms/FrmLogin.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,18 @@ namespace ProfSoft.AppForms
         public FrmLogin()
         {
             InitializeComponent();
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["Uri"]);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage respone = client.GetAsync("api/Permission/GetList").Result;
+                if (respone.IsSuccessStatusCode)
+                {
+                }
+            }
         }
     }
 }
---
+++ ProfSoft.DTO/Permission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfSoft.DTO
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
